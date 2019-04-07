af72335fc4652ff0a9af4361b22be6a4e041a6a9
Adrien VIDOT <adrien.vidot@gmail.com>
2019-04-07 14:58:27 +0200

    Remove test

--- tests/test_menu_backend.py
+++ tests/test_menu_backend.py
@@ -25,10 +25,3 @@ class TestMenu_backend(unittest.TestCase):
 
     def test_command_line_interface(self):
         """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'menu_backend.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
